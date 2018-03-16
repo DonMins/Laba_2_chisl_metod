7a6f1d61a5f7a7a46976ebf84b8420867d30c1ca
DonMins <maks000156@yandex.ru>
2018-03-16 21:37:16 +0400

    стыковка

--- MyClass.py
+++ MyClass.py
@@ -11,10 +11,10 @@ while x<=b:
     Tatulated.append(5*x*np.pow((5 * np.pi +2*x),-1/4))  # 12 вариант
     x+=0.01
     n+=1
-    
+
 #xarr = [i for i in range(300)] # 7 вариант
 xarr = [i for i in range(301)] # 12 вариант
-plt.figure("График какой то функции")
+plt.figure("График какой то функции прап")
 plt.plot(xarr,Tatulated)
 plt.grid()
 plt.show()
