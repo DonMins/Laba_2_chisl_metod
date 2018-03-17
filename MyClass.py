a4e6efee33b5548db16acfb3a5f7f99710cc0ee2
DonMins <maks000156@yandex.ru>
2018-03-17 19:31:53 +0400

    немного пличнее

--- MyClass.py
+++ MyClass.py
@@ -1,28 +1,30 @@
 import math as np
 import matplotlib.pyplot as plt
+
 Tatulated  = []
-a=0
+xarr =[]
 
-b=3
-#=a+0.01 # 7 вариант
-x=a      # 12 вариант
-n=0
-while x<=b:
-    #Tatulated.append(x/(10*np.pi *np.sin(x)))  # 7 вариант
-    Tatulated.append(5*x*np.pow((5 * np.pi +2*x),-1/4))  # 12 вариант
-    x+=0.01
-    n+=1
+def inputFunc(x):
+    return (x / (10 * np.pi * np.sin(x)))
 
-#xarr = [i for i in range(300)] # 7 вариант
-xarr = [i for i in range(301)] # 12 вариант
-plt.figure("График какой то функции прап")
-plt.plot(xarr,Tatulated)
-plt.grid()
-plt.show()
+def tabalatedFunc(a,b): # границы [a,b]
+ x = 0.1
+ Tatulated.append(x/(inputFunc(x)))
+ xarr.append(x)
+ x=1
+ while x<=b:
+    Tatulated.append(x/(10*np.pi *np.sin(x)))  # 7 вариант
+    xarr.append(x)
+    x+=1
+    #Tatulated.append(5*x*np.pow((5 * np.pi +2*x),-1/4))  # 12 вариант
 
+def plot ():
+    plt.figure("График какой то функции ")
+    plt.plot(xarr,Tatulated)
+    plt.grid()
+    plt.show()
 
-
-
-
+if __name__ == "__main__":
+    atom = ;
 
 
