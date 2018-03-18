8970a93d13864f0d5450053c38625faa3aba869b
DonMins <maks000156@yandex.ru>
2018-03-18 18:17:30 +0400

    1 и 2 задание

--- MyClass.py
+++ MyClass.py
@@ -6,19 +6,32 @@ import scipy as sp
 Tatulated = []
 xarr = []
 
-def inputFunc(x):
+def inputFunc(x): # исходная функция
     #return 5*x*np.pow((5 * np.pi +2*x),-1/4) # Таня
     return (x / (10 * np.pi * np.sin(x)))
 
+def MaxDx4Func(x): # функ-ция считает макс значение 4 производной
+    def csc(t,i):
+        return (1/np.sin(t))**i
+
+    def cot(t,i):
+        return (1 / np.tan(t)) ** i
+
+    return (1/(10*np.pi)) *( 4*(cot(x,3)*(-csc(x,1))-5*cot(x,1)*csc(x,3))+x*(5*csc(x,5)+cot(x,4)*csc(x,1)+
+                              18*cot(x,2)*csc(x,3)))
+
 def tabalatedFunc(a, b,h):  # границы [a,b], шаг
-    x = 0.00001
-    Tatulated.append(inputFunc(x))
-    xarr.append(x)
-    x = 1
+    x=a
+    if (x==0):
+        x += h
+        Tatulated.append(inputFunc(x))
+        xarr.append(x)
+        x += h
     while x <= b:
         Tatulated.append(inputFunc(x))
         xarr.append(x)
         x += h
+
 
 def plot():
     plt.figure("График какой то функции ")
@@ -26,10 +39,19 @@ def plot():
     plt.grid()
     plt.show()
 
-def  alpha():
+def  alpha(N):
     a =[[1,xarr[0],xarr[0]**2,xarr[0]**3],[1,xarr[1],xarr[1]**2,xarr[1]**3],
         [1,xarr[2],xarr[2]**2,xarr[2]**3],[1,xarr[3],xarr[3]**2,xarr[3]**3]]
-    b = numpy.zeros((4,1))
+    a = numpy.zeros(N,N)
+    for i in range(N):
+        a[i][0] = 1
+    for i in range(N):
+        for j in range(1,N):
+            a[i][j] =
+
+
+
+    b = numpy.zeros((N,1))
     for i in range(4):
         b[i][0] = Tatulated[i]
     return numpy.linalg.solve(a,b)
@@ -60,19 +82,28 @@ def linearSpline():
 
 
     return
+def OutPutTab(M):
+    print("       Табуляция функции")
+    print("--------------------------------")
+    for i in range(M):
+        print('|  x{0} = {1}   |   y{0} ={2}  |'.format(i,numpy.round(xarr[i], 5),
+                                      numpy.round(Tatulated[i], 5)))
 
 if __name__ == "__main__":
-    tabalatedFunc(0,3,1)
-    print(Tatulated)
-    print(xarr)
-
-    tmp = 2.5
-    ans = interpolPolynom(1)
-    print("Интерполяционный многочлен Лагранже в ", tmp, " =", float(interpolPolynom(tmp)))
+    tabalatedFunc(0,3,0.157)
+    OutPutTab(19)
+    MaxD4 = MaxDx4Func(3)
 
 
-    tmp = 2.5
-    print("Интерполяционный многочлен Ньютона в  ", tmp," =", interpolPolynomNewton(tmp))
+
+    #
+    # tmp = 2.5
+    # ans = interpolPolynom(1)
+    # print("Интерполяционный многочлен Лагранже в ", tmp, " =", float(interpolPolynom(tmp)))
+    #
+    #
+    # tmp = 2.5
+    # print("Интерполяционный многочлен Ньютона в  ", tmp," =", interpolPolynomNewton(tmp))
 
 
 
